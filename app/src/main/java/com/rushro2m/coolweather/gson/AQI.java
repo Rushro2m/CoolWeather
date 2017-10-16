8ba5641d2a037e195c9d2850481a9545b29da91c
IInmy <haoxujie1993@163.com>
2017-10-16 16:24:58 +0800

    CoolWeather

---
+++ app/src/main/java/com/rushro2m/coolweather/gson/AQI.java
@@ -0,0 +1,15 @@
+package com.rushro2m.coolweather.gson;
+
+public class AQI {
+
+    public AQICity city;
+
+    public class AQICity {
+
+        public String aqi;
+
+        public String pm25;
+
+    }
+
+}
